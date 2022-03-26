83ffc9c1cebc7aa1fbc9d342b6f0c81553115ea2
TOM BAÑADOS <tbanados@uc.cl>
2022-03-26 02:44:06 -0300

    started work on request module for pypond composer

---
+++ backend/pond_request.py
@@ -0,0 +1,30 @@
+import requests
+
+from private_parameters import url
+
+
+def put_score(score_string, score_type):
+    if score_type == "score":
+        extension = ""
+        data = {'score_data': score_string}
+    else:
+        extension = "instrument"
+        data = {'score_data': score_string,
+                'instrumet': score_type}
+
+    response = requests.put(url + extension, data=data)
+    print(response.status_code)
+
+
+def get_score(score_type):
+    if score_type == "score":
+        extension = ""
+        data = {}
+    else:
+        extension = "instrument"
+        data = {'instrumet': score_type}
+
+    response = requests.get(url + extension, data=data)
+    print(response.status_code)
+    json = response.json()
+    return json["score_data"]
