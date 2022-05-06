e170b11512012936e6dc3f32d291586e8bcbf000
TOM BAÑADOS <tbanados@uc.cl>
2022-05-06 14:16:04 -0400

    modified commands and composer data

--- parameters.py
+++ parameters.py
@@ -1,17 +1,22 @@
 SCORE_IMAGE_PATH = ["ly_files", "ly_files.png"]
 WINDOW_GEOMETRY = (200, 100, 1700, 900)
 BEAT_DURATION_MS = 1000
-COMMANDS = ['estirar_brazo', 'encoger_brazo', 'levantar_brazo', 'bajar_brazo', 'asentir', 'negar',
-            'girar', 'saltar', 'saludar', 'moverse_a_la_izquierda', 'moverse_a_la_derecha',
-            'abrir_bolsa', 'entregar_regalo', 'escuchar', 'tocar_un_instrumento', 'olvidar_algo',
-            'recordar_algo', 'tener_una_idea', 'comenzar_baile', 'terminar_baile', 'nadar',
-            'esconderse', 'esconder_objeto']
+COMMANDS = ['sentarse', 'leer', 'nadar', 'bailar', 'asentir', 'negar', 'saltar',
+            'esconderse', 'tocar_instrumento', 'levantar_brazo', 'bajar_brazo',
+            'remar', 'gritar', 'caminar', 'pensar', 'mover_a_derecha', 'mover_a_izquierda',
+            'abrazar', 'estirar', 'girar', 'imitar_animal', 'reír', 'señalar', 'revolver']
 USE_API = True
 
-COMMANDS_TEST = ["Estirar Brazo", "Recoger Brazo", "Levantar Brazo", "Bajar Brazo",
-                 "Asentir", "Negar", "Girar", "Saltar", "Saludar", "Moverse a la Izquierda",
-                 "Moverse a la Derecha",
-                 "Abrir bolsa", "Entregar Regalo", "Escuchar", "Tocar un Instrumento",
-                 "Olvidar Algo", "Recordar Algo",
-                 "Tener una Idea", "Comenzar Baile", "Terminar Baile", "Nadar", "Esconderse",
-                 "Esconder Objeto"]
+COMMANDS_TEST = ["Sentarse", "Leer", "Nadar", "Bailar", "Asentir", "Negar", "Saltar",
+                 "Esconderse", "Tocar Instrumento", "Levantar Brazo", "Bajar Brazo",
+                 "Remar", "Gritar", "Caminar", "Pensar", "Mover a Derecha",
+                 "Mover a Izquierda", "Abrazar", "Estirar", "Girar", "Imitar Animal",
+                 "Reír", "Señalar", "Revolver"]
+
+if __name__ == "__main__":
+    com = []
+    for command in COMMANDS_TEST:
+        new = command.replace(' ', '_')
+        new = new.lower()
+        com.append(new)
+    print(com)
